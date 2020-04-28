e0382715fcc1a747456b367c5e9a6957e32e3eb1
Awi Fatan <59038888+AwiFatan@users.noreply.github.com>
2020-04-28 23:08:10 +0700

    Update copytoclipboard.js

--- script/copytoclipboard.js
+++ script/copytoclipboard.js
@@ -1,6 +1,7 @@
  function klik() {
             var copyText = document.getElementById("af_pesan");
             copyText.select();
+            copyText.setSelectionRange(0, 99999)
             document.execCommand("copy");
 
         }
