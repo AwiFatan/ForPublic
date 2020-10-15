2e04355ed8b0ae18746450247f77c2ba2465d191
Awi Fatan <59038888+AwiFatan@users.noreply.github.com>
2020-10-15 17:27:36 +0700

    Create test.js

---
+++ testing/test.js
@@ -0,0 +1,4 @@
+var elementkiri = document.getElementById("kiri");
+var elementkanan = document.getElementById("kanan");
+elementkiri.classList.add("col-xl-3 col-lg-12 col-md-12");
+elementkanan.classList.add("col-xl-9 col-lg-12 col-md-12");
